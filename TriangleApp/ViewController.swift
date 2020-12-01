28a1b33c8d6efa895025c78f0b71d658fbcce461
Junia <junia.firdaus@lintasmediadanawa.com>
2020-12-01 20:24:15 +0700

    Initial Commit

---
+++ TriangleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriangleApp
+//
+//  Created by PT Lintas Media Danawa on 01/12/20.
+//  Copyright © 2020 JFS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
